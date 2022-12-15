67cb9b3d57c6b07202c6b6c72366498449c9d846
devCJH <http-@naver.com>
2022-12-15 21:54:30 +0900

    modify 폼 구현. 폼 보여주기 전에 게시물, 권한 체크

--- src/main/java/com/cjh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/cjh/exam/demo/controller/UsrArticleController.java
@@ -63,10 +63,6 @@ public class UsrArticleController {
 
 		Rq rq = (Rq) req.getAttribute("rq");
 
-		if (rq.getLoginedMemberId() == 0) {
-			return Utility.jsHistoryBack("로그인 후 이용해주세요");
-		}
-
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
@@ -81,22 +77,32 @@ public class UsrArticleController {
 
 		return Utility.jsReplace(Utility.f("%d번 게시물을 삭제했습니다", id), "list");
 	}
+	
+	@RequestMapping("/usr/article/modify")
+	public String showModify(HttpServletRequest req, Model model, int id) {
 
+		Rq rq = (Rq) req.getAttribute("rq");
+		
+		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
+
+		ResultData actorCanMDRd = articleService.actorCanMD(rq.getLoginedMemberId(), article);
+
+		if (actorCanMDRd.isFail()) {
+			return rq.jsReturnOnView(actorCanMDRd.getMsg(), true);
+		}
+		
+		model.addAttribute("article", article);
+		
+		return "usr/article/modify";
+	}
+	
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public ResultData<Article> doModify(HttpServletRequest req, int id, String title, String body) {
 
 		Rq rq = (Rq) req.getAttribute("rq");
 		
-		if (rq.getLoginedMemberId() == 0) {
-			return ResultData.from("F-A", "로그인 후 이용해주세요");
-		}
-
 		Article article = articleService.getArticle(id);
-
-//		if (article == null) {
-//			return ResultData.from("F-1", Utility.f("%d번 게시물은 존재하지 않습니다", id));
-//		}
 
 		ResultData actorCanModifyRd = articleService.actorCanMD(rq.getLoginedMemberId(), article);
 

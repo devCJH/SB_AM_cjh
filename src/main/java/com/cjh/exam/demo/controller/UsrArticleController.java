590d59d5887252764f060b7cda20083020c3b4fc
devCJH <http-@naver.com>
2022-12-19 21:47:42 +0900

    페이징처리 구현중

--- src/main/java/com/cjh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/cjh/exam/demo/controller/UsrArticleController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cjh.exam.demo.service.ArticleService;
@@ -22,7 +23,7 @@ public class UsrArticleController {
 	private ArticleService articleService;
 	private BoardService boardService;
 	private Rq rq;
-	
+
 	@Autowired
 	public UsrArticleController(ArticleService articleService, BoardService boardService, Rq rq) {
 		this.articleService = articleService;
@@ -34,11 +35,11 @@ public class UsrArticleController {
 	public String showWrite() {
 		return "usr/article/write";
 	}
-	
+
 	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
 	public String doWrite(int boardId, String title, String body) {
-
+		
 		if (Utility.empty(title)) {
 			return Utility.jsHistoryBack("제목을 입력해주세요");
 		}
@@ -49,25 +50,38 @@ public class UsrArticleController {
 		ResultData<Integer> writeArticleRd = articleService.writeArticle(rq.getLoginedMemberId(), boardId, title, body);
 
 		int id = (int) writeArticleRd.getData1();
-		
+
 		return Utility.jsReplace(Utility.f("%d번 글이 생성되었습니다", id), Utility.f("detail?id=%d", id));
 	}
 
 	@RequestMapping("/usr/article/list")
-	public String showList(Model model, int boardId) {
+	public String showList(Model model, @RequestParam(defaultValue = "1") int boardId,
+			@RequestParam(defaultValue = "1") int page) {
+
+		if (page <= 0) {
+			return rq.jsReturnOnView("페이지번호가 올바르지 않습니다", true);
+		}
 		
 		Board board = boardService.getBoardById(boardId);
 
 		if (board == null) {
 			return rq.jsReturnOnView("존재하지 않는 게시판입니다", true);
 		}
-		
+
 		int articlesCount = articleService.getArticlesCount(boardId);
-		List<Article> articles = articleService.getArticles(boardId);
+		
+		int itemsInAPage = 10;
+		
+		int pagesCount = (int) Math.ceil((double) articlesCount / itemsInAPage);
+		
+		List<Article> articles = articleService.getArticles(boardId, itemsInAPage, page);
 
 		model.addAttribute("board", board);
 		model.addAttribute("articles", articles);
 		model.addAttribute("articlesCount", articlesCount);
+		model.addAttribute("boardId", boardId);
+		model.addAttribute("page", page);
+		model.addAttribute("pagesCount", pagesCount);
 
 		return "usr/article/list";
 	}
@@ -88,7 +102,7 @@ public class UsrArticleController {
 
 		return Utility.jsReplace(Utility.f("%d번 게시물을 삭제했습니다", id), "list?boardId=1");
 	}
-	
+
 	@RequestMapping("/usr/article/modify")
 	public String showModify(Model model, int id) {
 
@@ -99,12 +113,12 @@ public class UsrArticleController {
 		if (actorCanMDRd.isFail()) {
 			return rq.jsReturnOnView(actorCanMDRd.getMsg(), true);
 		}
-		
+
 		model.addAttribute("article", article);
-		
+
 		return "usr/article/modify";
 	}
-	
+
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public String doModify(int id, String title, String body) {
@@ -116,9 +130,9 @@ public class UsrArticleController {
 		if (actorCanMDRd.isFail()) {
 			return Utility.jsHistoryBack(actorCanMDRd.getMsg());
 		}
-		
+
 		articleService.modifyArticle(id, title, body);
-		
+
 		return Utility.jsReplace(Utility.f("%d번 게시물을 수정했습니다", id), Utility.f("detail?id=%d", id));
 	}
 

6843b4c1cf92b2806533a1acc4a2a67613343e29
devCJH <http-@naver.com>
2022-12-27 21:08:02 +0900

    댓글 리스트 양식. 댓글 작성 처리 기능 구현중

---
+++ src/main/java/com/cjh/exam/demo/service/ReplyService.java
@@ -0,0 +1,25 @@
+package com.cjh.exam.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cjh.exam.demo.repository.ReplyRepository;
+import com.cjh.exam.demo.util.Utility;
+import com.cjh.exam.demo.vo.ResultData;
+
+@Service
+public class ReplyService {
+	
+	private ReplyRepository replyRepository;
+
+	@Autowired
+	public ReplyService(ReplyRepository replyRepository) {
+		this.replyRepository = replyRepository;
+	}
+
+	public ResultData<Integer> writeReply(int loginedMemberId, String relTypeCode, int relId, String body) {
+		replyRepository.writeReply(loginedMemberId, relTypeCode, relId, body);
+		int id = replyRepository.getLastInsertId();
+		return ResultData.from("S-1", Utility.f("%d번 댓글이 생성되었습니다", id), "id", id);
+	}
+}
